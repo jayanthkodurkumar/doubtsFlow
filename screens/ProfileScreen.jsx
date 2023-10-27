2752950cbd29dc581af3f0e8b73c39b13a9e119e
jayanthkodurkumar <jayanthkk1998@gmail.com>
2023-10-27 00:39:05 -0400

    Admin check done

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,4 +1,11 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
@@ -8,6 +15,7 @@ import {
   getDoc,
   getDocs,
   onSnapshot,
+  updateDoc,
 } from "firebase/firestore";
 import Navbar from "../components/Navbar";
 import { Image } from "react-native-elements";
@@ -19,6 +27,7 @@ const ProfileScreen = () => {
   const navigation = useNavigation();
   const user = useSelector((state) => state.auth.user);
   const [loggedUser, setLoggedUser] = useState([]);
+  const [adminCode, setAdminCode] = useState("");
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -44,17 +53,21 @@ const ProfileScreen = () => {
     };
   }, []);
   console.log(loggedUser);
-  if (!loggedUser || loggedUser.length === 0) {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Pressable style={styles.goBack} onPress={() => navigation.goBack()}>
-          <Icon name="arrow-back" size={30} color="gray" />
-        </Pressable>
-
-        <Text>Loading...</Text>
-      </SafeAreaView>
-    );
-  }
+  const code = () => {
+    const admincode = "12345";
+    const usersRef = collection(db, "users");
+    console.log("button pressed");
+    const userRef = doc(usersRef, loggedUser[0].id);
+    if (adminCode === "12345") {
+      updateDoc(userRef, { role: "admin" });
+      Alert.alert("Success", "Congratulations! You are now an admin.", [
+        { text: "OK", onPress: () => navigation.navigate("Home") },
+      ]);
+      navigation.navigate("Home");
+    } else {
+      Alert.alert("Failure", "Invalid Code", [{ text: "OK" }]);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -77,7 +90,7 @@ const ProfileScreen = () => {
         <Image
           style={styles.profilePic}
           source={{
-            uri: loggedUser[0].profilePic,
+            uri: loggedUser[0]?.profilePic,
           }}
         />
         <View style={styles.premiumIconContainer}>
@@ -97,9 +110,33 @@ const ProfileScreen = () => {
             <Text style={styles.value}>{userInfo.totalComments}</Text>
             <Text style={styles.key}>Luddies </Text>
             <Text style={styles.value}>{userInfo.luddies}</Text>
+            <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+              ENTER THE ADMIN CODE TO SWITCH TO ADMIN MODE
+            </Text>
+            <TextInput
+              placeholder="Your Code"
+              onChangeText={(enteredText) => {
+                setAdminCode(enteredText);
+              }}
+              style={styles.value}
+            ></TextInput>
+            <Pressable
+              onPress={() => code()}
+              style={{
+                backgroundColor: "#ea4335",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ color: "white", fontWeight: "bold" }}>Submit</Text>
+            </Pressable>
           </View>
         ))}
       </View>
+
       <Navbar homeIcon={true} settingsIcon={true} />
     </SafeAreaView>
   );
